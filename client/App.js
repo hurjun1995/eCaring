060f9aa520576c30f109289912e04672bae7d0f8
christinetrac <christineetrac@gmail.com>
2021-01-16 20:03:56 -0500

    cleanup

--- client/App.js
+++ client/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { DashboardStack } from "./navigation/DashboardStack";
 import { NavigationContainer } from '@react-navigation/native';
 
