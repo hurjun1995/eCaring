dd168b5f1d1f070056edc0b17d6a291a746f2c76
christinetrac <christineetrac@gmail.com>
2021-01-17 07:26:23 -0500

    touchup patient ui

--- client/App.js
+++ client/App.js
@@ -8,6 +8,33 @@ import {LogStack} from './routes/LogStack'
 export default function App() {
     const data = [
         {
+            key: new Date(2021, 0, 17, 10, 33, 30, 0),
+            value: {
+                hydration: 1.5,
+                foodIntake: ["Breakfast","Lunch", "Snack"],
+                vitals: "Stable",
+                medicine: [
+                    {name:"Insulin", dosage:400}
+                ],
+                mentalHealth: 5,
+                painLevel: 1,
+                notes: "Jane says she has been sleeping well."
+            }},
+        {
+            key: new Date(2021, 0, 16, 10, 33, 30, 0),
+            value: {
+                hydration: 2,
+                foodIntake: ["Breakfast","Lunch", "Dinner"],
+                vitals: "Stable",
+                medicine: [
+                    {name:"Insulin", dosage:400},
+                    {name:"Omega-3", dosage:250}
+                ],
+                mentalHealth: 4,
+                painLevel: 1,
+                notes: "Jane went for a walk today."
+        }},
+        {
             key: new Date(2021, 0, 15, 10, 33, 30, 0),
             value: {
                 hydration: 1.5,
@@ -37,16 +64,48 @@ export default function App() {
                 painLevel: 4,
                 notes: "Jane has been feeling down. Family should give her a call."
         }},
+        {
+            key: new Date(2021, 0, 13, 10, 33, 30, 0),
+            value: {
+                hydration: 2,
+                foodIntake: ["Dinner"],
+                vitals:"n/a",
+                medicine: [
+                    {name:"Ibuprofen", dosage:100},
+                    {name:"Omega-3", dosage:250},
+                    {name:"Hydro-codone", dosage:20},
+                    {name:"Metformin", dosage: 100}
+                ],
+                mentalHealth: 2,
+                painLevel: 4,
+                notes: "Jane has been feeling down. Family should give her a call."
+            }},
+        {
+            key: new Date(2021, 0, 12, 10, 33, 30, 0),
+            value: {
+                hydration: 2,
+                foodIntake: ["Dinner"],
+                vitals:"n/a",
+                medicine: [
+                    {name:"Ibuprofen", dosage:100},
+                    {name:"Omega-3", dosage:250},
+                    {name:"Hydro-codone", dosage:20},
+                    {name:"Metformin", dosage: 100}
+                ],
+                mentalHealth: 2,
+                painLevel: 4,
+                notes: "Jane has been feeling down. Family should give her a call."
+            }},
     ];
 
   return (
     <View style={styles.container}>
-        {/*<NavigationContainer>*/}
-        {/*  <DashboardStack data={data}/>*/}
-        {/*</NavigationContainer>*/}
         <NavigationContainer>
-            <LogStack/>
+          <DashboardStack data={data}/>
         </NavigationContainer>
+        {/*<NavigationContainer>*/}
+        {/*    <LogStack/>*/}
+        {/*</NavigationContainer>*/}
       <StatusBar style="auto" />
     </View>
   );
